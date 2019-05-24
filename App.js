fbdcffa472b81a238a77eab9eb06f13c342a57c5
Evan <egoff@vt.ewsd.org>
2019-05-24 08:25:50 -0400

    map

--- App.js
+++ App.js
@@ -1,21 +1,116 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Fragment } from 'react';
+import { Text, View, StyleSheet, Alert } from 'react-native';
+import { IntentLauncherAndroid, Location, MapView, Permissions } from 'expo';
+import { Constants } from 'expo';
 
-export default class App extends React.Component {
+// This is where we'll center our map if we don't have any other location to go to.
+const defaultLocation = {
+  latitude: 44.2612759,
+  longitude: -72.5835662,
+  latitudeDelta: 0.0922,
+  longitudeDelta: 0.0421,
+};
+
+export default class MapExample extends React.Component {
+
+  // Set the component's default state
+  state = { userLocation: {}, markerLocation: defaultLocation };
+
+  componentDidMount() {
+    // Get the user's current location (latitude and longitude)
+    this.getLocationAsync();
+  }
+
+
+  getLocationAsync = async () => {
+    // Ask for permissions to access the phone's GPS ( we only need to do this once)
+    const { status } = await Permissions.askAsync(Permissions.LOCATION);
+
+    // If we have permission then get the user's current location 
+
+    if (status === 'granted') {
+      const locationProviderStatus = await Location.getProviderStatusAsync();
+
+      // Check to see if the phone has GPS and it is enabled
+   
+      if (locationProviderStatus.locationServicesEnabled === false) {
+           // GPS is disabled  :-(  Put an annoying error message into the component's state to show later
+        this.setState({
+          errorMessage:
+            'Access to the device location is required. Please make sure you have location services on and you grant access when requested.',
+        });
+      } else {
+
+        // Huzzah, we have permission to use the GPS and it's enabled
+        // Let's get the user's current location (latitude and longitude)
+        const myLocation = await Location.getCurrentPositionAsync({});
+
+        //  Put the user's location into the component's state.
+        this.setState({ userLocation: myLocation });
+
+        // Start listening to the event that tells us when the user has changed location.
+        Location.watchPositionAsync(
+          { timeInterval: 3000, distanceInterval: 20 },
+          myLocation => {
+            // Update the component's state with the user's new location
+            this.setState({ errorMessage: null, userLocation: myLocation });
+          }
+        );
+      }
+    } else {
+          // Permission is not granted :-(  Put an annoying error message into the component's state to show later
+      this.setState({
+        errorMessage:
+          'Access to the device location is required. Please make sure you have location services on and you grant access when requested.',
+      });
+    }
+  };
+
+  // If the map region (area shown on the map, (lat, long and zoom level)) changes
+  // save it to component state so our map doesn't jump around when we re-render the map
+  onRegionChange = (region) => {
+    this.setState({ region });
+  }
+
+  // If the map is tapped, save the lat and long so we know where to put a pin
+  handleMapTap = event =>{this.setState({markerLocation: event.nativeEvent.coordinate})}
+
   render() {
+    // This is our map marker (Pin)
+    const marker = (
+      <MapView.Marker
+        coordinate={this.state.markerLocation}
+        description={'Tap Here'}
+        onCalloutPress={() => {
+          Alert.alert('Pin Pressed');
+        }}
+        pinColor={'green'}
+        stopPropagation={true}
+        title={'MY SPOT'}
+      />
+    );
+
+    // Render our map using all our component state variables
     return (
-      <View style={styles.container}>
-        <Text>coffee</Text>
-      </View>
+      <MapView
+        initialRegion={defaultLocation}
+        showsCompass={true}
+        showsMyLocationButton={true}
+        showsUserLocation={true}
+        region={this.state.region}
+        onRegionChange={this.onRegionChange}
+        onPress={this.handleMapTap}
+        style={{
+          height: '100%',
+          width: '100%',
+          padding: 0,
+          borderTopWidth: 0.5,
+          borderTopColor: '#AAA',
+        }}>
+        {marker}
+      </MapView>
     );
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
